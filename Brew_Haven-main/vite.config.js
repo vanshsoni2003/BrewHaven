2c8326352537e287de819360b544a01605c60d84
vanshsoni2003 <138280482+vanshsoni2003@users.noreply.github.com>
2025-11-14 16:32:15 +0530

    Add Brew Haven website initial frontend

--- Brew_Haven-main/vite.config.js
+++ Brew_Haven-main/vite.config.js
@@ -1,20 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // host: '192.168.1.59' , // Replace with your IP address
-    port: 3000, // You can specify any port you want
-  },
+  
+  // GitHub Pages ke liye BASE URL
+  base: "/Brew_Haven/", 
+
+  // server: {
+  //   // host: '192.168.1.59',   // (optional) mobile testing ke liye
+  //   port: 3000,
+  // },
 })
